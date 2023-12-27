52bdaa527cb52f7fa3fc09a7fa72218432466f36
Liv <110914989+livoszlak@users.noreply.github.com>
2023-12-27 15:38:51 +0100

    More style fixes

--- main.php
+++ main.php
@@ -9,21 +9,30 @@
     </div>
     <div class="rooms-wrapper">
         <a href="/views/room.php?room-type=1">
-            <div class="room-card">
+            <div class="room-card budget">
                 <div class="room-image"><img src="/assets/images/ROOM-pexels-ekaterina-bolovtsova-7445017.png"></div>
-                <div class="room-text"></div>
+                <div class="room-text">
+                    <div class="room-heading">BUDGET</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quibusdam animi voluptates ipsam eveniet provident accusamus architecto voluptas. Nisi doloremque architecto veniam nam optio cupiditate, facere repellendus iure repellat fuga? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus recusandae dignissimos eligendi ab, veniam nulla nemo sapiente voluptates dolor sed odio excepturi, pariatur similique. Id odit dolor nemo labore unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates amet vel vero! Provident laborum adipisci praesentium consequuntur fuga qui, veritatis nobis reprehenderit aliquid nihil corporis corrupti libero illo sequi facilis!</p>
+                </div>
             </div>
         </a>
         <a href="/views/room.php?room-type=2">
-            <div class="room-card">
+            <div class="room-card standard">
                 <div class="room-image"><img src="/assets/images/ROOM-pexels-ekaterina-bolovtsova-7445122.png"></div>
-                <div class="room-text"></div>
+                <div class="room-text">
+                    <div class="room-heading">STANDARD</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur totam illo nisi, similique fuga eius, iste ipsam enim possimus eveniet numquam quis animi itaque temporibus sequi earum, adipisci cumque? Perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor molestias, in illum quo quis consectetur, necessitatibus nesciunt ducimus corporis labore non voluptas minima, assumenda vitae possimus. Qui ex culpa magnam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quis possimus maiores aspernatur, laboriosam, accusamus exercitationem impedit quibusdam repellendus id et? Magni dolorem est repellendus blanditiis laborum. Dignissimos, eos fugiat.</p>
+                </div>
             </div>
         </a>
         <a href="/views/room.php?room-type=3">
-            <div class="room-card">
+            <div class="room-card luxury">
                 <div class="room-image"><img src="/assets/images/ROOM-pexels-max-rahubovskiy-6580369.png"></div>
-                <div class="room-text"></div>
+                <div class="room-text">
+                    <div class="room-heading">LUXURY</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, reprehenderit? Dicta provident nam assumenda illo magni facilis esse cupiditate. Voluptas quia distinctio aperiam nam commodi facere, tempora natus possimus delectus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero voluptas harum dolorum dolor esse ipsum, nemo omnis numquam nam debitis vel iste architecto corporis saepe temporibus est exercitationem eos eius? Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ut culpa impedit, quod, laudantium voluptatibus iusto, velit cumque vero necessitatibus natus officia quia totam dolor corrupti. Praesentium numquam mollitia totam.</p>
+                </div>
             </div>
         </a>
     </div>
