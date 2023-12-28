dd91249d91beaafbfb2dc8518acdda44b1be1807
Liv <110914989+livoszlak@users.noreply.github.com>
2023-12-28 20:38:39 +0100

    Booking response in HTML & link to JSON response added

--- views/booking-complete.php
+++ views/booking-complete.php
@@ -7,11 +7,24 @@ require __DIR__ . '/header.php'; ?>
 
 <main>
     <div class="thank-you">
-        <div class="info-headline">Booking complete</div>
+        <div class="headline">Booking complete</div>
+        <div class="booking-html">
+            You're spending <?= $_SESSION['totalDays']; ?> meowgical day/-s on The <?= $_SESSION['booking']['island']; ?><br>
+            The <?= $_SESSION['booking']['stars'] ?> star hotel <?= $_SESSION['booking']['hotel']; ?> welcomes you with happy paws!<br>
+            Arrival date: <?= $_SESSION['booking']['arrival_date']; ?><br>
+            Departure date: <?= $_SESSION['booking']['departure_date']; ?><br>
+            Total cost: <?= $_SESSION['booking']['total_cost']; ?><br>
+            Cats chosen for company during your stay: <?php for ($i = 0; $i < count($_SESSION['booking']['features']); $i++) : echo $_SESSION['booking']['features'][$i] . '! ';
+                                                        endfor; ?>
+        </div>
         <div class="json">
             <a href="/views/booking-response.php">Click here to see your booking in JSON format!</a>
         </div>
     </div>
 </main>
 
-<?php require __DIR__ . '/footer.php'; ?>
+<?php
+echo '<pre>';
+var_dump($_SESSION['booking']);
+
+require __DIR__ . '/footer.php'; ?>
